bb42b6962e25d0b21951020ecb2f794c2d40704e
dinhsonhai132 <dinhsonhai440@gmail.com>
2024-09-16 21:45:11 +0700

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ let cube = document.getElementById("cube");
 let gamearena = document.getElementById("gamearena");
 let bullet = document.getElementById("bullet");
 let score = document.getElementById("score");
+let point_score = document.getElementById("point")
+let point_x = Math.floor(Math.random() * gamearena.clientWidth - point_score.clientWidth)
+let point_y = 0
 let pos_x = 0;
-let pos_y = 0;
-let speed = 5;
+let pos_y = gamearena.clientHeight;
+let speed = 3;
 let jump = false;
 let ground = true;
 let height = 270;
@@ -22,14 +25,14 @@ window.addEventListener('keyup', (event) => {
     keys[event.key.toLowerCase()] = false;
 });
 
-function checkCollision() {
-    let cubeRect = cube.getBoundingClientRect();
-    let bulletRect = bullet.getBoundingClientRect();
+function checkCollision(a, b) {
+    let x = a.getBoundingClientRect();
+    let y = b.getBoundingClientRect();
 
-    if (bulletRect.left < cubeRect.right &&
-        bulletRect.right > cubeRect.left &&
-        bulletRect.top < cubeRect.bottom &&
-        bulletRect.bottom > cubeRect.top) {
+    if (y.left < x.right &&
+        y.right > x.left &&
+        y.top < x.bottom &&
+        y.bottom > x.top) {
             return true;
     } else {
         return false;
@@ -69,28 +72,43 @@ function move(event) {
     } else if (pos_x > gamearena.clientWidth - cube.clientWidth) {
         pos_x = gamearena.clientWidth - cube.clientWidth;
     }
-
+    bull_y -= 5;
     if (pos_y < 0) {
         pos_y = 0;
     } else if (pos_y > gamearena.clientHeight - cube.clientHeight) {
         pos_y = gamearena.clientHeight - cube.clientHeight;
         ground = true;
     }
-    bull_y -= 5;
+    
     if (bull_y < 0) {
         bull_y = gamearena.clientWidth - bullet.clientWidth;
-        point += 5;
-        score.textContent = 'score: ' + point;
     }
-    if (checkCollision()) {
+    if (checkCollision(cube, bullet)) {
         bull_y = gamearena.clientWidth - bullet.clientWidth;
         point -= 1;
         score.textContent = 'score: ' + point;
     }
+
+    point_y += 5
+    if (point_y > gamearena.clientHeight - point_score.clientHeight) {
+        point_y = 0
+        point_x = Math.floor(Math.random() * gamearena.clientWidth)
+    }
+
+    if (checkCollision(cube, point_score)) {
+        point += 5
+        score.textContent = 'score: ' + point;
+        point_y = 0
+        point_x = Math.floor(Math.random() * gamearena.clientWidth)
+    }
+
     bullet.style.left = bull_y + 'px'
     cube.style.left = pos_x + 'px';
     cube.style.top = pos_y + 'px';
+    point_score.style.left = point_x + 'px';
+    point_score.style.top = point_y + 'px';
     requestAnimationFrame(move);
 }
+
 bullet.style.top = bull_x + 'px'
 move()
